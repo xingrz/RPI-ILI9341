155a9f2da3ed3c896795be7d31a4d7c4e9820933
XiNGRZ <hi@xingrz.me>
2021-04-14 14:59:28 +0800

    Update pinout

--- ili9341/src/pinout.h
+++ ili9341/src/pinout.h
@@ -1,10 +1,10 @@
 #ifndef __ILI9341_PINOUT__
 #define __ILI9341_PINOUT__
 
-#define GPIO(N) (N)
+#include <bcm2835.h>
 
-#define PIN_LCD_DC GPIO(25)
-#define PIN_LCD_RST GPIO(27)
-#define PIN_LCD_BL GPIO(18)
+#define PIN_LCD_DC RPI_GPIO_P1_22
+#define PIN_LCD_RST RPI_GPIO_P1_18
+#define PIN_LCD_BL RPI_GPIO_P1_21
 
 #endif  // __ILI9341_PINOUT__
